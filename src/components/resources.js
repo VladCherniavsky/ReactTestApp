a4f5cd4c8937c859b0c2548e2fdd7fc5883b67a7
Vlad Cherniavsky <Vlad Cherniavsky>
2016-05-04 17:43:04 +0300

    simple routing

---
+++ src/components/resources.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      Super special recipe
+      <ul>
+        <li>Sugar</li>
+        <li>Pepper</li>
+        <li>Salt</li>
+      </ul>
+    </div>
+  );
+}
